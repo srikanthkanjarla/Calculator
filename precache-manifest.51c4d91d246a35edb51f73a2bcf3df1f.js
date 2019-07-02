1ce679a335f5331af20cd25382ea9563cf784f05
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-07-03 00:30:50 +0530

    Updates

---
+++ precache-manifest.51c4d91d246a35edb51f73a2bcf3df1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6454332a2eadf36b12f3",
+    "url": "/static/js/main.6454332a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1fddef54029499ffd396",
+    "url": "/static/js/2.1fddef54.chunk.js"
+  },
+  {
+    "revision": "cf1ced744186a3f15caf431f3e674eba",
+    "url": "/index.html"
+  }
+];
