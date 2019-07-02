222b0e18151d04e1e5601a7b691c626046c7cc42
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-07-02 23:24:23 +0530

    Add code for functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,126 +4,200 @@ import Display from './Display';
 import Key from './Key';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      userInput:'',
-      expression:'',             
-      result:'0',
-    }
+    this.state = {
+      displayValue: '0',
+      operator: null,
+      allowDot: true,
+      firstValue: '',
+      secondValue: '',
+      nextValue: false,
+    };
     this.handleClick = this.handleClick.bind(this);
-    this.clearDisplay = this.clearDisplay.bind(this);
-    this.deleteLastDigit = this.deleteLastDigit.bind(this);
   }
-  
-  clearDisplay(){
-    this.setState({
-      result:'0'
-    })
-  }
-  
-    //delete last character on bakcspace key
-    deleteLastDigit(){
-      if(this.state.result !== ''){
-        if(this.state.result.length === 1 && this.state.result !== '0'){
-        this.clearDisplay();
-        console.log('clear display');
-        }else if(this.state.result.length >= 1 && this.state.result !== '0'){
-          this.setState((state)=>({
-            result:state.result.slice(0,-1),
-          }))
-          console.log('remove last char');
+
+  handleClick(event) {
+    const { displayValue, operator, allowDot, firstValue, secondValue, nextValue } = this.state;
+    const { value } = event.target;
+    const operatorList = ['+', '-', '*', '/', '%'];
+    const isEndWithDot = displayValue[displayValue.length - 1] === '.';
+    const isEndWithOperator = operatorList.includes(displayValue[displayValue.length - 1]);
+
+    switch (value) {
+      case 'clear':
+        this.setState({
+          displayValue: '0',
+          allowDot: true,
+          firstValue: '',
+          secondValue: '',
+          operator: null,
+          nextValue: false,
+        });
+        break;
+
+      case 'del':
+        if (secondValue) {
+          this.setState({
+            secondValue: secondValue.length === 1 ? '' : secondValue.slice(0, -1),
+          });
+        } else if (operator) {
+          this.setState({
+            operator: null,
+          });
+        } else if (firstValue) {
+          this.setState({
+            firstValue: firstValue.length === 1 ? '' : firstValue.slice(0, -1),
+          });
         }
-      }
-     }
-  handleClick(event){
-    let value = event.target.value;
-    let digitKeys = ['0','1','2','3','4','5','6','7','8','9'];
-    let operatorKeys = ['+','-','*','/','%','='];
-    let periodKey=['.'];
 
-    if(digitKeys.includes(value)){
-      console.log('Hey I am a number');
+        if (displayValue !== '' && displayValue !== '0') {
+          this.setState({
+            displayValue: displayValue.length === 1 ? '0' : displayValue.slice(0, -1),
+            allowDot: !isEndWithDot,
+          });
+        }
+
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.setState({
+          displayValue: displayValue === '0' ? value : displayValue + value,
+        });
+        if (!nextValue) {
+          this.setState({
+            firstValue: firstValue + value,
+          });
+        } else {
+          this.setState({
+            secondValue: secondValue + value,
+          });
+        }
+        break;
+
+      case '+':
+      case '-':
+      case '/':
+      case '*':
+      case '%':
+        this.setState({
+          operator: value,
+          allowDot: true,
+          nextValue: true,
+          displayValue: (isEndWithOperator || isEndWithDot ? displayValue.slice(0, -1) : displayValue) + value,
+        });
+        if (nextValue && secondValue && secondValue !== '.') {
+          // eslint-disable-next-line no-eval
+          const result = eval(`${firstValue}${operator}${secondValue}`);
+          const formatResult = (result % 1 === 0 ? result : result.toFixed(3)).toString();
+          this.setState({
+            displayValue: formatResult + value,
+            firstValue: formatResult,
+            secondValue: '',
+            operator: value,
+          });
+        }
+        break;
+
+      case '.':
+        this.setState({
+          allowDot: false,
+          displayValue: allowDot ? displayValue + value : displayValue,
+        });
+
+        if (!nextValue) {
+          this.setState({
+            firstValue: allowDot ? firstValue + value : firstValue,
+          });
+        } else {
+          this.setState({
+            secondValue: allowDot ? secondValue + value : secondValue,
+          });
+        }
+        break;
+
+      case '=':
+        if (nextValue && secondValue && secondValue !== '.') {
+          // eslint-disable-next-line no-eval
+          const result = eval(`${firstValue}${operator}${secondValue}`);
+          const formatResult = (result % 1 === 0 ? result : result.toFixed(3)).toString();
+          this.setState({
+            displayValue: formatResult,
+            firstValue: formatResult,
+            secondValue: '',
+            nextValue: false,
+            operator: null,
+            allowDot: result % 1 === 0,
+          });
+        }
+
+        break;
+      default:
     }
+  }
 
-    if(operatorKeys.includes(value)){
-      console.log('Hey I am an operator');
-    }
-
-    if(periodKey.includes(value)){
-      console.log("hey I am period");
-    }
-
-
-    if(this.state.result === '0' && !operatorKeys.includes(value)){
-      this.setState({
-        result:value,
-      }) 
-    }
-
-    if(this.state.result !== '0'){    
-      this.setState((state)=>({
-        result:state.result + value,
-      }))
-    } 
-   }
-
- 
   render() {
-    const{classes}=this.props;
-    const{result}=this.state;
+    const { classes } = this.props;
+    const { displayValue } = this.state;
     return (
       <div className={classes.app}>
-      <div className={classes.container}>
-      <Display result={result}/>
-      <Key label={'C'} handleClick={this.clearDisplay}/>      
-      <Key label={'%'} handleClick={this.handleClick}/>
-      <Key label={'/'} handleClick={this.handleClick}/>
-      <Key label={'←'} handleClick={this.deleteLastDigit}/>
-      <Key label={7}   handleClick={this.handleClick}/>
-      <Key label={8}   handleClick={this.handleClick}/>
-      <Key label={9}   handleClick={this.handleClick}/>
-      <Key label={'*'} handleClick={this.handleClick}/>
-      <Key label={4}   handleClick={this.handleClick}/>
-      <Key label={5}   handleClick={this.handleClick}/>
-      <Key label={6}   handleClick={this.handleClick}/>
-      <Key label={'-'} handleClick={this.handleClick}/>
-      <Key label={1}   handleClick={this.handleClick}/>
-      <Key label={2}   handleClick={this.handleClick}/>
-      <Key label={3}   handleClick={this.handleClick}/>
-      <Key label={'+'} handleClick={this.handleClick}/>
-      <Key label={'GT'}handleClick={this.handleClick}/>
-      <Key label={0}   handleClick={this.handleClick}/>
-      <Key label={'.'} handleClick={this.handleClick}/>
-      <Key label={'='} handleClick={this.handleClick}/>
-      </div>  
+        <div className={classes.container}>
+          <Display result={displayValue} />
+          <Key value="clear" label="C" handleClick={this.handleClick} splBtn />
+          <Key value="%" label="%" handleClick={this.handleClick} />
+          <Key value="/" label="&divide;" handleClick={this.handleClick} />
+          <Key value="del" label="&laquo;" handleClick={this.handleClick} splBtn />
+          <Key value={7} label={7} handleClick={this.handleClick} />
+          <Key value={8} label={8} handleClick={this.handleClick} />
+          <Key value={9} label={9} handleClick={this.handleClick} />
+          <Key value="*" label="&times;" handleClick={this.handleClick} />
+          <Key value={4} label={4} handleClick={this.handleClick} />
+          <Key value={5} label={5} handleClick={this.handleClick} />
+          <Key value={6} label={6} handleClick={this.handleClick} />
+          <Key value="-" label="-" handleClick={this.handleClick} />
+          <Key value={1} label={1} handleClick={this.handleClick} />
+          <Key value={2} label={2} handleClick={this.handleClick} />
+          <Key value={3} label={3} handleClick={this.handleClick} />
+          <Key value="+" label="+" handleClick={this.handleClick} />
+          <Key value="gt" label="GT" handleClick={this.handleClick} />
+          <Key value={0} label={0} handleClick={this.handleClick} />
+          <Key value="." label="." handleClick={this.handleClick} />
+          <Key value="=" label="=" handleClick={this.handleClick} splBtn/>
+        </div>
       </div>
     );
   }
 }
-const styles ={
-  app:{
-     height:'100vh',
-     width:'100vw',
-     display:'flex',
-     justifyContent:'center',
-     alignItems:'center',
-     backgroundColor:'#e8e9f3',
-     margin:0,
-   },
-  container:{
-    width:350,
-    height:500,
-    borderRadius:'12px',
-    backgroundColor:'hsla(244,16.5%,17.8%,0.5)',
-    display:'grid',
-    gridTemplateColumns:'auto auto auto auto',
-    justifyItems:'center',
-    alignItems:'space-evenly',
-    overflow:'none'
-  }
-
-  
-}
+const styles = {
+  app: {
+    height: '100vh',
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e8e9f3',
+    margin: 0,
+  },
+  container: {
+    width: 350,
+    height: 500,
+    borderRadius: '12px',
+    backgroundColor: 'hsla(244,16.5%,17.8%,0.5)',
+    display: 'grid',
+    gridTemplateColumns: 'auto auto auto auto',
+    justifyItems: 'center',
+    alignItems: 'space-evenly',
+    overflow: 'none',
+  },
+};
 
 export default injectSheet(styles)(App);
